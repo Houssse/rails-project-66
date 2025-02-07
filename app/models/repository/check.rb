eaafb00b5750ba3494cb40d3fe81a4deab6b5020
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2025-02-08 00:40:25 +0300

    add aasm to check

--- app/models/repository/check.rb
+++ app/models/repository/check.rb
@@ -2,6 +2,25 @@
 
 module Repository
   class Check < ApplicationRecord
-    belongs_to :repository, class_name: 'Repository::Base'
+    belongs_to :repository
+
+    include AASM
+
+    aasm column: 'state' do
+      state :checking, initial: true
+      state :passed, :failed
+
+      event :mark_as_passed do
+        transitions from: :checking, to: :passed
+      end
+
+      event :mark_as_failed do
+        transitions from: :checking, to: :failed
+      end
+
+      event :restart_check do
+        transitions from: %i[passed failed], to: :checking
+      end
+    end
   end
 end
