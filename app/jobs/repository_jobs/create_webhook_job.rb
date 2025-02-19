0beeae5c9881d2ab5fcc7351c5ae9908fcd8bd0e
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2025-02-19 22:29:15 +0300

    fix lint

--- app/jobs/repository_jobs/create_webhook_job.rb
+++ app/jobs/repository_jobs/create_webhook_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module RepositoryJobs
   class CreateWebhookJob < RepositoryJobs::ApplicationJob
     queue_as :default
--- test/jobs/create_webhook_job_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CreateWebhookJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
