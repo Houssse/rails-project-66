5a64570f3fbb5c1efe24663d434e902c1b02048d
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2025-02-23 12:02:14 +0300

    route refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,21 @@ Rails.application.routes.draw do
   # test sentry
   get 'sentry_test', to: 'application#test_sentry'
 
-  # omniauth
-  post 'auth/:provider', to: 'web/auth#request', as: :auth_request
-  get 'auth/:provider/callback', to: 'web/auth#callback', as: :callback_auth
-  delete 'logout', to: 'web/auth#destroy', as: :logout
 
   scope module: :web do
     resources :repositories, only: %i[index show new create] do
       resources :checks, only: %i[show create], module: :repository
     end
+
+    # omniauth
+    post 'auth/:provider', to: 'auth#request', as: :auth_request
+    get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
+    delete 'logout', to: 'auth#destroy', as: :logout
+
+    root 'home#index'
   end
 
   namespace :api do
     resources :checks, only: [:create]
   end
-
-  root 'web/home#index'
 end
